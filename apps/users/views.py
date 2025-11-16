6ed155b38bdc6040bd39dc2bd8a16d75ed4de514
styse <shmadad0@gmail.com>
2025-11-16 17:36:14 +0330

    Refactor and add the knox authentication to the  RegisterView and the LoginView

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,23 +1,49 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
+from knox.models import AuthToken
+from knox.views import LogoutView as KnoxLogoutView
+from knox.views import LogoutAllView as KnoxLogoutAllView
 from .serializers import UserRegisterSerializer, UserLoginSerializer
-from rest_framework.authtoken.models import Token
+
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        token = AuthToken.objects.create(user)[1]
+
+        return Response({
+            "user": {
+                "id": user.id,
+                "email": user.email,
+                "phone": user.phone,
+            },
+            "token": token
+        }, status=status.HTTP_201_CREATED)
+
 
 class LoginView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data['user']
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        token = AuthToken.objects.create(user)[1]
+
+        return Response({
+            "user": {
+                "id": user.id,
+                "email": user.email,
+                "phone": user.phone,
+            },
+            "token": token
+        }, status=status.HTTP_200_OK)
+
