2a582ffcb314a70b5441ea6885a2d1bf0b58ef75
styse <shmadad0@gmail.com>
2025-11-16 15:00:59 +0330

    Categorize INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -30,15 +30,29 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "users",
 ]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
+LOCAL_APPS = [
+    "apps.users",
+]
+
+INSTALLED_APPS = [
+    *DJANGO_APPS,
+    *THIRD_PARTY_APPS,
+    *LOCAL_APPS,
+]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
