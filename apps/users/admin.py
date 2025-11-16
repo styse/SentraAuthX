3d4b453154db2e04822533559d76aae9e940c85a
styse <shmadad0@gmail.com>
2025-11-16 15:20:43 +0330

    Add the UserAdmin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['id', 'phone', 'email', 'is_staff', 'is_active']
+    search_fields = ['phone', 'email']
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'email', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+
+    # This allows login with phone instead of username
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
