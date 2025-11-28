180db0edf8752ba19d3d7a7c649055b2c0d00f67
styse <shmadad0@gmail.com>
2025-11-28 23:56:37 +0330

    Add otp endpoints

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, LogoutAllView
+from .views import (
+    RegisterView, LoginView, LogoutView,
+    LogoutAllView, RequestOTPView, VerifyOTPView
+    )
 
 urlpatterns = [
+    # Auth Endpoints
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('logoutall/', LogoutAllView.as_view(), name='logoutall'),
+    
+    # OTP Endpoints
+    path('otp/request/', RequestOTPView.as_view(), name='otp-request'),
+    path('otp/verify/', VerifyOTPView.as_view(), name='otp-verify'),
 ]
