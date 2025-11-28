08c958829696be033b689ec4e971d602547f8ca6
styse <shmadad0@gmail.com>
2025-11-28 23:57:38 +0330

    Add `RequestOTPView` and `VerifyOTPView`

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,10 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
 from knox.models import AuthToken
 from knox.views import LogoutView as KnoxLogoutView
 from knox.views import LogoutAllView as KnoxLogoutAllView
-from .serializers import UserRegisterSerializer, UserLoginSerializer
+from .serializers import (
+    UserRegisterSerializer, UserLoginSerializer,
+    OTPRequestSerializer, OTPVerifySerializer
+    )
+from .models import OTP
 
 
 class RegisterView(APIView):
@@ -55,3 +60,33 @@ class LogoutView(KnoxLogoutView):
 class LogoutAllView(KnoxLogoutAllView):
     permission_classes = ()
     pass
+
+
+
+class RequestOTPView(APIView):
+    def post(self, request):
+        serializer = OTPRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            otp_code = OTP.generate_otp()
+            OTP.objects.create(user=user, code=otp_code)
+            
+            # TODO: send otp_code via SMS or Email
+            print(f"OTP for {user}: {otp_code}")  # For testing
+            
+            return Response({"detail": "OTP sent successfully"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VerifyOTPView(APIView):
+    def post(self, request):
+        serializer = OTPVerifySerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            otp = serializer.validated_data['otp']
+            
+            otp.is_used = True
+            otp.save()
+            
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
