03f2cce88f0bbc589f5bca1e6ea98440f69b26bf
styse <shmadad0@gmail.com>
2025-12-01 18:14:08 +0330

    Change the phone field to phone_number

--- apps/users/models.py
+++ apps/users/models.py
@@ -45,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.email or self.phone or str(self.id)
+        return self.email or self.phone_number or str(self.id)
 
 
 
