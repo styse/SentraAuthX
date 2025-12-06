16bebb20806dd842e20b546716978ed30f807cc5
styse <shmadad0@gmail.com>
2025-12-06 16:59:59 +0330

    Create UserSessionListView class

--- apps/user_sessions/views.py
+++ apps/user_sessions/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import SessionSerializer
+from .models import Session
 
-# Create your views here.
+
+class UserSessionListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = SessionSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        
+        active_only = self.request.query_params.get['active_only']
+        qs = Session.objects.filter(user=user)
+        
+        if active_only == 'true':
+            qs = qs.filter(is_active=True)
+            
+        return qs.order_by("-last_active_at")
