91f4cd9e5fa89ffc4f92f910b7ae7a3916e53785
styse <shmadad0@gmail.com>
2025-12-07 14:10:43 +0330

    Add "user-session-delete" path

--- apps/user_sessions/urls.py
+++ apps/user_sessions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (
-    UserSessionListView
+    UserSessionListView, UserSessionDeleteView
 )
 
 urlpatterns = [
     path("list/", UserSessionListView.as_view(), name = 'user-session-list'),
+    path("delete/<uuid:session_id>", UserSessionDeleteView.as_view(), name= 'user-session-delete'),
 ]
