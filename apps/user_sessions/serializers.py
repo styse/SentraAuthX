ea39cf9f066d72de8ab819acdd7169a848204d2a
styse <shmadad0@gmail.com>
2025-12-08 18:11:46 +0330

    Add `get_device` and `get_is_current` functions to the SessionSerializer

--- apps/user_sessions/serializers.py
+++ apps/user_sessions/serializers.py
@@ -3,15 +3,28 @@ from .models import Session
 
 
 class SessionSerializer(serializers.ModelSerializer):
+    device = serializers.SerializerMethodField()
+    is_current = serializers.SerializerMethodField()
+
     class Meta:
         model = Session
         fields = [
-            'id',
-            'user',
-            'token_key',
-            'ip_address',
-            'user_agent',
-            'created_at',
-            'last_active_at',
-            'is_active'
+            "id",
+            "token_key",
+            "ip_address",
+            "device",
+            "user_agent",
+            "created_at",
+            "last_active_at",
+            "is_active",
+            "is_current",
         ]
+
+    def get_device(self, obj):
+        return obj.get_parsed_device()
+
+    def get_is_current(self, obj):
+        request = self.context.get("request")
+        if not request or not request.auth:
+            return False
+        return obj.token_key == str(request.auth)[:32]
